751f14a2e313c8f69c95f974961140f78c257fc4
max ohsawa <max.ohsawa@gmail.com>
2023-03-11 16:10:42 -0500

    exports function to convert csv to object

--- csvToJSObject.js
+++ csvToJSObject.js
@@ -1,22 +1,17 @@
 import fs from 'fs'
 
-const data = await fs.promises.readFile('./test.csv', 'utf8')
-
-const rows = data.split('\r\n')
-
-const headers = rows[0].split(',')
-
-const output = []
-
-for (let i = 1; i < rows.length; i++) {
-    const [saiNum, ...flags] = rows[i].split(',')
-
-    const record = {
-        saiNum,
-        flags,
+export default async function convertCSVtoObject(filePath) {
+    const data = await fs.promises.readFile(filePath, 'utf8')
+    const rows = data.split('\r\n')
+    const output = []
+    for (let i = 1; i < rows.length; i++) {
+        const [saiNum, ...flags] = rows[i].split(',')
+        const record = {
+            saiNum,
+            flags,
+        }
+        output.push(record)
     }
 
-    output.push(record)
+    return output
 }
-
-console.log(output)
